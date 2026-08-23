db77c2d82ad76b00a2a66bbe5ec824ebda1d3cbe
unlockpro8558 <unlockpro8558@gmail.com>
2026-08-23 09:47:23 +0530

    Turn into PWA and update title

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Let the browser do its default thing
+  // For offline support, you'd add caching logic here
+});
