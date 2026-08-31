425fe0080b00f0495f5617efcd8f8d75da958dab
surya5887 <unlockpro8558@gmail.com>
2026-08-31 10:14:53 +0530

    UI: Re-design Defaulters List mobile cards to be colorful, remove overlap bug, and add proper WhatsApp icon

--- fix_index_css.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/index.css', 'utf8');
-
-code = code.replace('@page {\n    margin: 1cm;', '@page {\n    margin: 0;');
-
-fs.writeFileSync('src/index.css', code);
-console.log('Fixed @page margin');
--- fix_toolbar_again.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/components/TransferCertificatePrintView.tsx', 'utf8');
-
-code = code.replace(/maxWidth: '210mm',\s*minHeight: '297mm',\s*margin: '0 auto 16px auto', background: 'white'/, "maxWidth: '800px', margin: '0 auto 16px auto', background: 'white'");
-
-fs.writeFileSync('src/components/TransferCertificatePrintView.tsx', code);
-console.log('Fixed toolbar');
