d714dd4bda31ac75b5f7d16960abf6f0ea524f21
surya5887 <suryapandit5887@gmail.com>
2026-09-12 20:09:55 +0530

    CHORE: Ultimate cleanup of ALL temporary scripts and unused files in frontend root

--- force_import.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/pages/SystemSettings.tsx', 'utf8');
-
-if (!content.includes('import WhatsAppSetup')) {
-    content = "import WhatsAppSetup from './WhatsAppSetup';\nimport { MessageSquare } from 'lucide-react';\n" + content;
-    fs.writeFileSync('src/pages/SystemSettings.tsx', content);
-    console.log("Forced import");
-}
--- test_billing.ts
+++
@@ -1,9 +0,0 @@
-import { runAutomatedBilling } from "./src/services/billingService";
-
-async function run() {
-  console.log("Running billing engine...");
-  const count = await runAutomatedBilling();
-  console.log("Generated count:", count);
-  process.exit(0);
-}
-run();
